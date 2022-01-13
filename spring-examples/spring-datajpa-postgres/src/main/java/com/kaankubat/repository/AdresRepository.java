a0f66273f1dec31b61b3f5f45e8245f4c607bb35
Kaan <170103074@ogr.alanya.edu.tr>
2022-01-14 01:30:52 +0300

    Update | DataJPA ile PostgreSQL veritabani kullanarak REST API geliştirme.

---
+++ spring-examples/spring-datajpa-postgres/src/main/java/com/kaankubat/repository/AdresRepository.java
@@ -0,0 +1,8 @@
+package com.kaankubat.repository;
+
+import com.kaankubat.entity.Adres;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdresRepository extends JpaRepository<Adres, Long> {
+
+}
---
+++ spring-examples/spring-datajpa-postgres/src/main/java/com/kaankubat/repository/KisiRepository.java
@@ -0,0 +1,7 @@
+package com.kaankubat.repository;
+
+import com.kaankubat.entity.Kisi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KisiRepository extends JpaRepository<Kisi, Long> {
+}
